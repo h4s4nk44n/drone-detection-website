1681ddd22ff67ee258db44acb3f606689c0ae87c
h4s4nk44n <hasankaan5151@gmail.com>
2025-05-31 03:10:41 +0300

    download_cookies.sh fix

--- backend/download_cookies.sh
+++ backend/download_cookies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Downloading cookies.txt from GCS..."
-gsutil cp gs://drone-detection-secrets/cookies.txt /app/cookies.txt
+curl -o /app/cookies.txt https://storage.googleapis.com/drone-detection-secrets/cookies.txt
